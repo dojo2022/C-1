53794358b36be7fb66f8240349959c9491a5f315
dojo2022-suzukiyu <suzuki-yuu-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-13 15:32:11 +0900

    Update List.java

--- src/src/model/List.java
+++ src/src/model/List.java
@@ -1,7 +1,57 @@
 package model;
 import java.io.Serializable;
+import java.util.Date;
 
 //javabeans
 public class List implements Serializable {
+	private int number;
+	private Date date;
+	private String id;
+
+
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public boolean isCheck_tf() {
+		return check_tf;
+	}
+	public void setCheck_tf(boolean check_tf) {
+		this.check_tf = check_tf;
+	}
+	private boolean check_tf;
+
+
+	public List(int number, Date date, String id, boolean check_tf) {
+		super();
+		this.number = number;
+		this.date = date;
+		this.id = id;
+		this.check_tf = check_tf;
+	}
+
+	public List() {
+		super();
+		this.number = 0;
+		this.date = null;
+		this.id = "";
+		this.check_tf = false;
+	}
 
 }
+
+
