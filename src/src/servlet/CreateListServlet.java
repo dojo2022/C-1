709ad807a79d95620ae0268dd53e0f27e4326721
dojo2022-inoue-wataru <inoue-wataru-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-13 18:08:34 +0900

    議事録等

--- src/src/servlet/CreateListServlet.java
+++ src/src/servlet/CreateListServlet.java
@@ -29,7 +29,7 @@ public class CreateListServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		response.sendRedirect("/osilis/ListServlet");
 	}
 
