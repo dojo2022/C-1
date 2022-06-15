cab7617d682ae7ae338e98a0e6c5a9298a1357d9
dojo2022-motohashi <103239458+dojo2022-motohashi@users.noreply.github.com>
2022-06-15 16:48:35 +0900

    Update EventDAO.java

--- src/src/dao/EventDAO.java
+++ src/src/dao/EventDAO.java
@@ -1,5 +1,122 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 public class EventDAO {
 
+	//イベントの登録
+	public boolean eventRegist(String event, int type, int level, String user_id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C1", "sa", "");
+
+			// SQL文を準備する
+			String sql = "INSERT INTO events(event,type,level,user_id) VALUES(?,?,?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setString(1,event);
+			pStmt.setInt(2,type);
+			pStmt.setInt(3,level);
+			pStmt.setString(4,user_id);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+
+			catch (SQLException e) {
+				e.printStackTrace();
+				result = false;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				result = false;
+
+			}
+			finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					result = false;
+				}
+			  }
+		    }
+			//結果を返す
+			return result;
+		}
+
+
+	//イベントの編集（更新）
+
+	public boolean eventEdit(String event, int type, int level, int available, String user_id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C1", "sa", "");
+
+			// SQL文を準備する
+			String sql = "UPDATE events set event=?,type=?,level=?,available=?,user_id=? where NUMBER=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setString(1,event);
+			pStmt.setInt(2,type);
+			pStmt.setInt(3,level);
+			pStmt.setInt(4,available);
+			pStmt.setString(5,user_id);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+
+			catch (SQLException e) {
+				e.printStackTrace();
+				result = false;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				result = false;
+
+			}
+			finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					result = false;
+				}
+			  }
+		    }
+			//結果を返す
+			return result;
+		}
+
 }
+
+
