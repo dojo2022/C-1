c290d7a6288ccd5ac0b1aac7b3267083536a9bbd
dojo2022-motohashi <103239458+dojo2022-motohashi@users.noreply.github.com>
2022-06-16 16:14:29 +0900

    Update UserEditServlet.java

--- src/src/servlet/UserEditServlet.java
+++ src/src/servlet/UserEditServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class UserEditServlet
@@ -21,6 +22,12 @@ public class UserEditServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//現在のユーザー情報を取得
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("id") == null) {
+					response.sendRedirect("/osilis/LoginServlet");
+					return;
+				}
 
 		// イベント編集画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/userEdit.jsp");
