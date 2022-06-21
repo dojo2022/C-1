36d866b9d44e52f913eca6931c86751aedf40cc5
dojo2022-motohashi <103239458+dojo2022-motohashi@users.noreply.github.com>
2022-06-21 17:00:28 +0900

    Update EventDAO.java

--- src/src/dao/EventDAO.java
+++ src/src/dao/EventDAO.java
@@ -189,6 +189,71 @@ public class EventDAO {
 			//結果を返す
 			return result;
 		}
+	//引数にInteger型Listをいれて、listの中に入っているevent_numに対応するevents_levelとポイントを取得
+	public List<Integer> pointCheck(List<Integer> clearEventNum) {
+		Connection conn = null;
+		List<Integer> list = new ArrayList<>();
+		int point = 0;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C1", "sa", "");
+
+			for(int i = 0 ; i<clearEventNum.size() ;i++) {
+				int clear = clearEventNum.get(i);
+				// SQL文を準備する
+				String sql = "SELECT number,event,events.level,code,point FROM EVENTS  inner join events_level on events.level"
+						+ " = events_level.code where number = ?;";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1,clear);
+	;
+				//検索条件にタイプと難易度の記載があるが、INTをStringに変える必要あり？
+
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					point=rs.getInt("point");
+					list.add(point);
+				}
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			list = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			list = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					list = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return list;
+	}
+
+
+
 
 }
 
