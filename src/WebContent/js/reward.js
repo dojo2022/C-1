6fc6ab3b1e67f8234b830d493eb47912bef42e7a
dojo2022-inoue-wataru <inoue-wataru-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-23 17:13:11 +0900

    経験値バー苦戦中

--- src/WebContent/js/reward.js
+++ src/WebContent/js/reward.js
@@ -9,17 +9,24 @@ var bar = new ProgressBar.Line(container, {
   strokeWidth: 4,//進捗ゲージの太さ
   easing: 'easeInOut',//アニメーション効果
   duration: 1400,//時間指定
-  color: '#FFEA82',
+  color: '#008b8b',
   trailColor: '#eee',//ゲージベースの色
   trailWidth: 1,//ゲージベースの太さ
   svgStyle: {width: '100%', height: '100%'}
 });
 
 
-
-
-//xにDBのカウントの値を入れる
-let x
+//xに今日の獲得ポイントを入れる
+let x=150;
 
 //かっこの中身が?/100。
-bar.animate(60/100);
+if(x<100){
+	bar.animate(x/100);
+}else{
+	while(x>100){
+		bar.animate(1);
+
+      x -= x-100
+    bar.animate(0);
+	}
+}
