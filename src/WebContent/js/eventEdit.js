7b41faf2d746ceb7c87d7d9bb716cb1624fc488f
dojo2022-otsuji <103238489+dojo2022-otsuji@users.noreply.github.com>
2022-06-28 04:37:39 +0900

    event

--- src/WebContent/js/eventEdit.js
+++ src/WebContent/js/eventEdit.js
@@ -6,3 +6,21 @@
 			let element = document.getElementById(ele.name);
 			element.value = ele.value;
 		}
+
+/*document.getElementById('form1').onsubmit = function () {
+ 			const eventName = document.getElementById('form1').Event.value;
+
+			if (eventName === '') {
+				window.alert('イベント名は必須項目です！');
+				return false;
+			}
+		}
+
+document.getElementById('form2').onsubmit = function () {
+ 			const eventEditName = document.getElementById('form2').Event_Edit.value;
+
+			if (eventEditName === '') {
+				window.alert('イベント名は必須項目です！');
+				return false;
+			}
+		}*/
