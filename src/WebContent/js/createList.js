be4b126e6a1529af618d649576f62373de5fdbbb
dojo2022-inoue-wataru <inoue-wataru-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-27 09:56:47 +0900

    Update createList.js

--- src/WebContent/js/createList.js
+++ src/WebContent/js/createList.js
@@ -16,8 +16,8 @@ if(dow === 0 || dow === 6){
 	formHtml += '</form>';
 }else if(0<dow && dow<6){
 	formHtml =	'<form method="post" action="/osilis/CreateListServlet">';
-	formHtml +=	'<input type="radio" id="weekday" value="平日" name="week" class="weekday" checked><label for="weekday">平日</label>';
-	formHtml +=	'<input type="radio" id="weekend" value="休日" name="week" class="weekend"><label for="weekend" >休日</label><br>';
+	formHtml +=	'<input type="radio" id="weekday" value="平日" name="week" class="weekday"><label for="weekday">平日</label>';
+	formHtml +=	'<input type="radio" id="weekend" value="休日" name="week" class="weekend" checked><label for="weekend" >休日</label><br>';
 	formHtml +=	'<input type="submit" id="weeklist" value="予定の作成">';
 	formHtml += '</form>';
 }
